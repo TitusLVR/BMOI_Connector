d74dd449907f0cfc578341e59485a32af28aff3a
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2023-05-07 08:25:58 +0900

    fix: Remove unneeded function

--- MOI3D/Commands/bmoi_Import_OBJ.js
+++ MOI3D/Commands/bmoi_Import_OBJ.js
@@ -1,18 +1,4 @@
 // ImportObj v.1.5 - Max Smirnov. 2015 + Modified version by Titus (Titus.mailbox@gmail.com)
-function getMethods(obj) {
-	var result = [];
-	for (var id in obj) {
-	  try {
-		if (typeof(obj[id]) == "function") {
-		  result.push(id + ": " + obj[id].toString());
-		}
-	  } catch (err) {
-		result.push(id + ": inaccessible");
-	  }
-	}
-	return result;
-  }
-
 function importObj()
 {
 	var MoI_dir = moi.filesystem.getAppDataDir();
